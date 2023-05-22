37c92b13c5e80f73de967f4ba4ff74d2c0dd77a8
Lucca <kontakt@luccabaumgaertner.de>
2023-05-22 17:52:00 +0200

    exercise 1 fix types

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -7,6 +7,7 @@ HTTP_TIMEOUT = 10000
 DATA_URL = "https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv"
 SQL_PATH = "/airports.sqlite"
 
+
 df = pd.read_csv(DATA_URL, delimiter=';', index_col='column_1')
 print(df.head())
 print(df.info())
@@ -21,10 +22,10 @@ df.to_sql("airports","sqlite://" + SQL_PATH, if_exists='replace', dtype={
     "column_4": Text,
     "column_5": String(3),
     "column_6": String(4),
-    "column_7": Float,
-    "column_8": Float,
+    "column_7": Text,
+    "column_8": Text,
     "column_9": Integer,
-    "column_10": String,
-    "column_11": String(1),
+    "column_10": Float,
+    "column_11": Text(1),
     "column_12": String,
     "geo_punkt": String})
