05a0aaff3bd204b2f54e6c954414811b87697d10
Lucca <kontakt@luccabaumgaertner.de>
2023-05-22 17:55:07 +0200

    fix imports and add comments

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,20 +1,21 @@
+# imports
 import pandas as pd
-from sqlalchemy.types import Integer, Text, String, DateTime, Float
-from sqlalchemy import create_engine
+from sqlalchemy.types import Integer, Text, String, Float
 
+# define constants
 HTTP_TIMEOUT = 10000
-
 DATA_URL = "https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv"
 SQL_PATH = "/airports.sqlite"
 
 
+# download the raw data
 df = pd.read_csv(DATA_URL, delimiter=';', index_col='column_1')
-print(df.head())
-print(df.info())
+# print(df.head())
+# print(df.info())
 
-print(SQL_PATH)
+# print(SQL_PATH)
 
-
+# save data to sqlite db, setting the correct types
 df.to_sql("airports","sqlite://" + SQL_PATH, if_exists='replace', dtype={
     "column_1": Integer,
     "column_2": Text,
