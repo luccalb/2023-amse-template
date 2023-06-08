bed1721869c5aa4efae986b808a69c465b50e33c
Lucca <kontakt@luccabaumgaertner.de>
2023-06-08 16:54:28 +0200

    Fix unit tests

--- data/unit_tests.py
+++ data/unit_tests.py
@@ -1,13 +1,20 @@
 import os
 import sys
-import data_pipeline
+import json
+from data_extractor import DataExtractor
 
 abs_path = os.path.dirname(__file__)
 
 EXAMPLE_CSV_URL = "https://www.w3schools.com/python/pandas/data.csv"
 EXPECTED_LOCATION = os.path.join(abs_path, "raw/test_data.csv")
+CONFIG_FILE = os.path.join(abs_path, "pipeline_config.json")
 
-data_pipeline.save_url(EXAMPLE_CSV_URL, EXPECTED_LOCATION)
+with open(CONFIG_FILE, encoding='UTF-8') as config_json:
+        config = json.load(config_json)
+
+ex = DataExtractor(config)
+
+ex.save_url(EXAMPLE_CSV_URL, EXPECTED_LOCATION)
 
 file_exists = os.path.exists(EXPECTED_LOCATION)
 
