5362c442d21939905807210edb6301300376d856
Lucca <kontakt@luccabaumgaertner.de>
2023-06-17 11:02:41 +0200

    exercise 3 save to sqlite

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -11,17 +11,39 @@ df = pd.read_csv(DATA_URL, delimiter=';', encoding='latin1', skiprows=6, skipfoo
 df = df.iloc[:, [0,1,2,12,22,32,42,52,62,72]]
 df.columns = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
 
-# convert CIN column to string because of possible leading zero
-df['CIN'] = df['CIN'].astype(str)
-
 # drop empty cells
-df.dropna(inplace=True)
+df = df.dropna(how='any', subset=df.columns)
+
+# drop all rows with value '-'
+for col in df.columns:
+    df = df[df[col] != '-']
+    
+print(df.head())
+print(df.info())
+
+# convert CIN column to string because of possible leading zero
+df = df.astype({'date': 'str',
+                'CIN': 'str',
+                'name': 'str',
+                'petrol': 'int32',
+                'diesel': 'int32',
+                'gas': 'int32',
+                'electro': 'int32',
+                'hybrid': 'int32',
+                'plugInHybrid': 'int32',
+                'others': 'int32'})
+
 
 
 # validate CIN numbers
 df = df[df['CIN'].str.match(r"[0-9]{4,5}")]
 
-print(df.info())
+
+
+# validate all integer columns > 0
+int_cols = ['petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
+df[int_cols] = df[int_cols][df[int_cols] > 0]
+
 
 # save data to sqlite db, setting the correct types
 df.to_sql("cars","sqlite://" + SQL_PATH, if_exists='replace', dtype={
