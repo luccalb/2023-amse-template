47f6ca8aeb15ae31e99ef56a3be67f994fb50dbe
Lucca <kontakt@luccabaumgaertner.de>
2023-06-17 10:28:21 +0200

    exercise 3 CIN validation

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,10 +1,37 @@
 import pandas as pd
+from sqlalchemy.types import Integer, Text, String, Float
+
 
 DATA_URL = "https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv"
+SQL_PATH = "/cars.sqlite"
+
 
 df = pd.read_csv(DATA_URL, delimiter=';', encoding='latin1', skiprows=6, skipfooter=4, engine='python')
 
 df = df.iloc[:, [0,1,2,12,22,32,42,52,62,72]]
 df.columns = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
 
-print(df.head())
+# convert CIN column to string because of possible leading zero
+df['CIN'] = df['CIN'].astype(str)
+
+# drop empty cells
+df.dropna(inplace=True)
+
+
+# validate CIN numbers
+df = df[df['CIN'].str.match(r"[0-9]{4,5}")]
+
+print(df.info())
+
+# save data to sqlite db, setting the correct types
+df.to_sql("carrs","sqlite://" + SQL_PATH, if_exists='replace', dtype={
+    "date": Text,
+    "CIN": String(5),
+    "name": Text,
+    "petrol": Integer,
+    "diesel": Integer,
+    "gas": Integer,
+    "electro": Integer,
+    "hybrid": Integer,
+    "plugInHybrid": Integer,
+    "others": Integer})
